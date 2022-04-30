30cce072ab4fcaa315d2e1fdf41599d1ce63641f
Michelle Caraballo <caraballomp@yahoo.com>
2022-04-30 10:50:24 -0700

    commit tree a38d1d0d44d58277aee05e4f7dbf74ed8d6c4c34

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,17 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
-    render json: students
+    if params[:name]
+      students = Student.where("lower(first_name) = ? or lower(last_name) = ?", params[:name].downcase, params[:name].downcase)
+    
+        render json: students
+      else 
+        render json: students
+    end
   end
 
+    def show
+      student = Student.where(id: params[:id]).first
+      render json: student
+    end
 end
